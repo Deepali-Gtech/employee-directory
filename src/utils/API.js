3ef06e757ca435d6ace24eb693331da3c51edab8
Deepali Garg <deepaligarg.chd@gmail.com>
2021-05-01 20:48:35 -0400

    suppress the lint warning

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   getEmployees: function () {
     return axios.get("https://randomuser.me/api?results=10&nat=us");
