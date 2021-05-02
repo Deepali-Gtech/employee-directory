82645b4b43a53d402556f797d595b57b917b6378
Deepali Garg <deepaligarg.chd@gmail.com>
2021-05-01 20:41:38 -0400

    added buttons and sort functionality

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,8 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
-  getEmployees: function() {
-    return axios.get("https://randomuser.me/api?results=10");
+  getEmployees: function () {
+    return axios.get("https://randomuser.me/api?results=10&nat=us");
   },
+  
 };
